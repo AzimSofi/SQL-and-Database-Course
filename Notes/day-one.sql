352e089ef705d9e76ff9aa8480c4eef77ee91dc0
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-03-29 17:03:13 +0800

    Organize files and code

---
+++ Notes/day-one.sql
@@ -0,0 +1,8 @@
+INT -- Whole number
+DECIMAL(M,N) -- M: no. digit before decimal, N: no. digit after decimal
+VARCHAR(1) -- str len = 1
+BLOB -- Binary Large Object (imag, file)
+DATE -- YY-MM-DD
+TIMESTAMP -- YYYY-MM-DD HH:MM:SS
+
+-- Create table
