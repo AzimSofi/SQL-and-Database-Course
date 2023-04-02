a22ec5cbb32eff8066f74baedbdb1da8a794f726
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-02 16:23:42 +0800

    Update and rename table-used.sql to database-used.sql

--- table-used.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO student(name,major) VALUES('Jack', 'Biology');
-INSERT INTO student(name,major) VALUES('Kate', 'Sociology');
-INSERT INTO student(name,major) VALUES('Claire', 'Chemistry');
-INSERT INTO student(name,major) VALUES('Jack', 'Biology');
-INSERT INTO student(name,major) VALUES('Mike', 'Computer Science');
