ba69de6baf50a221f3663c1531c5de40876095af
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-02 16:22:18 +0800

    Create table-used.sql

---
+++ table-used.sql
@@ -0,0 +1,5 @@
+INSERT INTO student(name,major) VALUES('Jack', 'Biology');
+INSERT INTO student(name,major) VALUES('Kate', 'Sociology');
+INSERT INTO student(name,major) VALUES('Claire', 'Chemistry');
+INSERT INTO student(name,major) VALUES('Jack', 'Biology');
+INSERT INTO student(name,major) VALUES('Mike', 'Computer Science');
