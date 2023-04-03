d5327df1ecc15d9b3944febb95c09c7ae564dc5e
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-03 18:50:09 +0800

    Update database-used.sql

--- database-used.sql
+++ database-used.sql
@@ -2,7 +2,7 @@ CREATE TABLE employee (
   emp_id INT PRIMARY KEY,
   first_name VARCHAR(40),
   last_name VARCHAR(40),
-  birth_day DATE, -- (DD-MM,YY)
+  birth_day DATE, -- 'YYYY-MM-DD'
   sex VARCHAR(1), -- (F)emale or (M)ale
   salary INT,
   super_id INT, -- Foreign key
