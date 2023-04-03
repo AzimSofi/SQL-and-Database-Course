caaffeb559d0a550c58c9534c82f01660ab55d45
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-03 18:50:39 +0800

    Update day-one.sql

--- Notes/day-one.sql
+++ Notes/day-one.sql
@@ -2,5 +2,5 @@ INT -- Whole number
 DECIMAL(M,N) -- M: no. digit before decimal, N: no. digit after decimal
 VARCHAR(1) -- str len = 1
 BLOB -- Binary Large Object (imag, file)
-DATE -- YY-MM-DD
+DATE -- YYYY-MM-DD
 TIMESTAMP -- YYYY-MM-DD HH:MM:SS
