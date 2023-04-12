313bd984f74844cc3d259a990b6e17cdc68aa13b
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-12 21:25:48 +0800

    Update to-view-and-drop-table.sql

--- to-view-and-drop-table.sql
+++ to-view-and-drop-table.sql
@@ -2,3 +2,14 @@ use database_name; -- to select a database
 
 SHOW TABLES;
 DROP TABLE table_name;
+
+-- If it gives an error command 
+ALTER TABLE works_with DROP FOREIGN KEY works_with_ibfk_1;
+ALTER TABLE client DROP FOREIGN KEY client_ibfk_1;
+ALTER TABLE employee DROP FOREIGN KEY employee_ibfk_1, DROP FOREIGN KEY employee_ibfk_2;
+ALTER TABLE branch DROP FOREIGN KEY branch_ibfk_1;
+
+DROP TABLE works_with;
+DROP TABLE client;
+DROP TABLE employee;
+DROP TABLE branch;
